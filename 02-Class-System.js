1dd8273938c2ce8b5d2c62446a13351028b0b569
hristo2612 <hristoapps@gmail.com>
2018-05-10 14:34:03 +0300

    modified: 02-Class-System file - for better understanding

--- 02-Class-System.js
+++ 02-Class-System.js
@@ -11,9 +11,12 @@ Ext.define('MyApp.Session', {
 });
 
 var session = Ext.create("MyApp.Session", {
+    title: 'a nice title'
 });
 
 session.setTitle('chrises awesome session');
+session.setDescription('somenice description');
 
 console.log(session.getTitle());
+console.log(session.getDescription());
 
