aa15517cbf359c50050d33165b738e5ef80018ec
hristo2612 <hristoapps@gmail.com>
2018-05-10 14:12:24 +0300

    modified: viewport for 03-Base-ExtJS-App

--- 03-Base-ExtJS-App.js
+++ 03-Base-ExtJS-App.js
@@ -1,10 +1,6 @@
 Ext.application({
     name: 'MyApp',
     launch: function() {
-        Ext.create('Ext.container.ViewPort', {
-            items: [
-                
-            ]
-        });
+        Ext.Viewport
     }
 })
